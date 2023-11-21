a62497376be3173915477213b9d6ffa360eb7343
mihir.bangia <mihir.bangia@krishtechnolabs.com>
2023-11-21 18:00:43 +0530

    login added with backend

--- src/components/login/loginform.js
+++ src/components/login/loginform.js
@@ -1,156 +1,170 @@
-import React from "react";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
 import { useForm } from "react-hook-form";
-import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
-import { useState } from "react";
-import { auth } from "../firebase/firebase";
-import { Toaster, toast } from "react-hot-toast";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import DrawerAppBar from "../Navbar/navbar";
+import { ToastContainer, toast } from "react-toastify";
 
-export default function Loginform() {
-  const [showOtp, setShowOtp] = useState(false);
+export default function SignIn() {
+  const navigate = useNavigate();
+console.log(document.cookie)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const [user, setUser] = useState();
-
-  const { register, handleSubmit } = useForm();
-
-  const sendOtp = async (data) => {
-    if (!window.recaptchaVerifier) {
-      window.recaptchaVerifier = new RecaptchaVerifier(
-        auth,
-        "recaptcha-container",
-        {
-          size: "invisible",
-        }
-      );
-    }
-    const appVerifier = window.recaptchaVerifier;
-
-    const phoneNumber = "+91" + data.no;
-    await signInWithPhoneNumber(auth, phoneNumber, appVerifier)
-      .then((confirmationResult) => {
-        window.confirmationResult = confirmationResult;
-        toast.success("OTP Sended Sucessfully");
-        setShowOtp(true);
-      })
-      .catch((error) => {
-        toast.error(error.message);
-      });
+  const notify = (message, condition) => {
+    condition(message, {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",  
+    });
   };
 
-  const verify = (data) => {
-    window.confirmationResult
-      .confirm(data.otp)
-      .then(async (result) => {
-        // User signed in successfully.
-        const user = result.user;
-        setUser(user.uid);
-        // setLoading(false);
-      })
-      .catch((error) => {
-        console.log(error.message);
-        toast.error(error.message);
-      });
+  const onSubmit = async (data) => {
+    let response = await axios.post(
+      "http://localhost:4001/login",
+      {
+        password: data.password,
+        email: data.email,
+      },
+      { withCredentials: true }
+    );
+    if (response.data === "login success") {
+      notify('Login Successful',toast.success)
+     setTimeout(() => {
+      navigate("/");
+     }, 2000); 
+    } else {
+      notify('Unable to login', toast.error)
+    }
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          {/* <LockOutlinedIcon /> */}
-        </Avatar>
-        <Typography component="h1" variant="h5" sx={{ color: "white" }}>
-          {user ? user : "Sign in"}
-        </Typography>
-        {!showOtp && (
-          <Box
-            component="form"
-            onSubmit={handleSubmit(sendOtp)}
-            noValidate
-            sx={{ mt: 2 }}
-          >
-            <TextField
-              margin="normal"
-              variant="standard"
-              fullWidth
-              type={"number"}
-              id="mobile"
-              label="Mobile No."
-              name="mobile"
-              autoFocus
-              InputProps={{ style: { color: "white" } }}
-              {...register("no", {
-                required: "This is a required field",
-                minLength: 10,
-              })}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Send OTP
-            </Button>
-          </Box>
-        )}
-      </Box>
-      {showOtp && (
-        <Box
-          component="form"
-          onSubmit={handleSubmit(verify)}
-          noValidate
-          sx={{ mt: 2 }}
-        >
-          <TextField
-            margin="normal"
-            variant="standard"
-            fullWidth
-            type={"number"}
-            id="otp"
-            label="OTP"
-            name="otp"
-            autoFocus
-            InputProps={{ style: { color: "white" } }}
-            {...register("otp", {
-              required: "This is required field",
-              minLength: 6,
-            })}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Verify
-          </Button>
-        </Box>
-      )}
-      <div
-        style={{ display: "none" }}
-        className="recaptcha-container"
-        id="recaptcha-container"
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={1500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
       />
-      <Toaster position="top-center" reverseOrder={false} />
-    </Container>
-    // <Toaster/>
+
+      <DrawerAppBar />
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage:
+              "url(https://source.unsplash.com/random?wallpapers)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit(onSubmit)}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                {...register("email", { required: "Email is required" })}
+              />
+              <span className="error-message">{errors.name?.message}</span>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                {...register("password", { required: "Password is required" })}
+              />
+              <span className="error-message">{errors.password?.message}</span>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link
+                    href="#"
+                    variant="body2"
+                    onClick={() => navigate("/register")}
+                  >
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
   );
 }
