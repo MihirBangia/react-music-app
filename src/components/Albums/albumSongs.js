2792f122b84b6298c3896e8d9df8373994990cf0
mihir.bangia <mihir.bangia@krishtechnolabs.com>
2023-12-05 18:32:18 +0530

    albumdetail page half done

--- src/components/Albums/albumSongs.js
+++ src/components/Albums/albumSongs.js
@@ -1,15 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import {useLocation} from 'react-router-dom';
+import './albumsongs.css'
 
 export default function AlbumSongs() {
     const location = useLocation();
+    const [songsinalbum,setsongsinalbum] = useState([]);
   async function getAlbumSongs(){
     let res = await axios.get(`https://saavn.me/albums?link=${location.state.link}`);
-    console.log(res);
+    setsongsinalbum(res.data.data);
   }
   useEffect(()=>{
     getAlbumSongs()
   },[])
-  return <div></div>;
+  console.log(songsinalbum)
+  return(
+  <div className='main'>
+ <div className="album-section">
+      <img className="album-cover" src={songsinalbum?.image[2].link} alt="Album Cover" />
+      <div className="album-info">
+        <h1>{songsinalbum?.name}</h1>
+      </div>
+    </div>
+
+    <ul className="song-list">
+      <li className="song-item">
+        <span>1. Song 1</span>
+        <span>3:45</span>
+      </li>
+      <li className="song-item">
+        <span>2. Song 2</span>
+        <span>4:20</span>
+      </li>
+    </ul>
+
+  </div>
+  );
 }
